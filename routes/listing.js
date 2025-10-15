6b7fd96f2336b308d2efabb11076d6d41e712016
mustafiya <mustafiyasheikh14@gmail.com>
2025-10-15 21:04:22 +0530

    Initial commit - Wanderlust project

---
+++ routes/listing.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//index router
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+//new router
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+//create router
+router.post(
+  "/",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    const newListing = new Listing(req.body.Listings);
+
+    await newListing.save();
+    console.log("listing added", newListing);
+    res.redirect("/listings");
+  })
+);
+
+//show router
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listings: listing });
+  })
+);
+
+//edit router
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//update router
+router.put(
+  "/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listings);
+    // { new: true, runValidators: true }
+    res.redirect("/listings");
+  })
+);
+
+//destroy router
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
