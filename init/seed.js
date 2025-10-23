0a9db228f161d6663592b16cc172678ee192d9e1
mustafiya <mustafiyasheikh14@gmail.com>
2025-10-23 16:12:00 +0530

    Fix: corrected category

--- init/seed.js
+++ init/seed.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-const Listing = require('../models/listing.js');
-const { data } = require('./data');
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const { data } = require("./data");
 
-mongoose.connect('mongodb://127.0.0.1:27017/wanderlust')
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wanderlust")
   .then(async () => {
-    console.log("DB connected");
+    console.log("MONGODB_ATLAS connected");
     await Listing.deleteMany({});
     await Listing.insertMany(data);
     console.log("Seeded all listings!");
     mongoose.connection.close();
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
--- init/seedCategories.js
+++ init/seedCategories.js
@@ -1,34 +1,35 @@
 const mongoose = require("mongoose");
-const Listing = require("../models/listing.js"); 
+const Listing = require("../models/listing.js");
 const Category = require("../models/categories.js"); // your Category model
-const categoryData = require("./categorydata.js"); 
+const categoryData = require("./categorydata.js");
 
-mongoose.connect("mongodb://127.0.0.1:27017/wanderlust", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wanderlust", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-async function seedCategories () {
-    await Category.deleteMany({});
-    for (const category of categoryData) {
-        const listingIds = [];
-        for (const title of category.listings){
-            const listing = await Listing.findOne({ title });
-            if (listing){
-                listingIds.push(listing._id);
-            }
-        }
-        const newCategory = new Category({
-            name: category.name,
-            listings: listingIds
-        });
-        await newCategory.save();
-        console.log(newCategory);
+async function seedCategories() {
+  await Category.deleteMany({});
+  for (const category of categoryData) {
+    const listingIds = [];
+    for (const title of category.listings) {
+      const listing = await Listing.findOne({ title });
+      if (listing) {
+        listingIds.push(listing._id);
+      }
     }
-    console.log("all categories added successfully ");
-    mongoose.connection.close();
+    const newCategory = new Category({
+      name: category.name,
+      listings: listingIds,
+    });
+    await newCategory.save();
+    console.log(newCategory);
+  }
+  console.log("all categories added successfully ");
+  mongoose.connection.close();
 }
 
 seedCategories();
